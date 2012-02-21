082d54cd7e36e9006d83b09002da4f212edacf27
Alvaro Vila <alvarovmz@gmail.com>
2012-02-21 18:05:39 +0100

    Cruce

--- src/callejero/Cruce.java
+++ src/callejero/Cruce.java
@@ -9,5 +9,20 @@ package callejero;
  * @author roberto
  */
 public class Cruce {
+
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+
+    public Cruce (int x1, int y1, int x2, int y2){
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
     
+    public void paint(Graphics g) {
+        g.drawLine(this.x1, this.y1, this.x2, this.y2);
+    }
 }
