8022f9d6842ec989f5cb5a22a5312eb515b6e0fd
Alvaro Vila <alvarovmz@gmail.com>
2012-04-13 16:51:30 +0200

    New package tourism

---
+++ src/tourism/Avion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tourism;
+
+import cartograph.Transporte;
+/**
+ *
+ * @author alvaro
+ */
+public class Avion extends Transporte {
+    
+}
---
+++ src/tourism/Hotel.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tourism;
+
+import cartograph.Spot;
+/**
+ *
+ * @author alvaro
+ */
+public class Hotel extends Spot {
+    
+    public Hotel  (int x, int y, int c) {
+        super(x,y,c);
+    }
+    
+}
