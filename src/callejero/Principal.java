104dbab52cfdd4ecd180ca3b49848d751c18a3b3
Alvaro Vila <alvarovmz@gmail.com>
2012-05-04 17:01:05 +0200

    Fix maps

--- src/callejero/Principal.java
+++ src/callejero/Principal.java
@@ -25,25 +25,24 @@ public class Principal {
 
     
     public void test() {
-        Cruce c1 = new Cruce (25,665, "Lisboa");
-        Cruce c2 = new Cruce (140,670, "Madrid");
-        Cruce c3 = new Cruce (235,655, "Barcelona");
-        Cruce c4 = new Cruce (270,500, "París");
-        Cruce c5 = new Cruce (240,430, "Londres");
-        Cruce c6 = new Cruce (155,380, "Dublín");
-        Cruce c7 = new Cruce (60,100, "Reikiavik");
-        Cruce c8 = new Cruce (170,350, "Belfast");
-        Cruce c9 = new Cruce (215,325, "Edimburgo");
-        Cruce c10 = new Cruce (425,630, "Roma");
-        Cruce c11 = new Cruce (750,280, "Moscú");
-        Cruce c12 = new Cruce (340,615, "Mónaco");
-        Cruce c13 = new Cruce (390,575, "Milán");
-        Cruce c14 = new Cruce (310,450, "Bruselas");
-        Cruce c15 = new Cruce (325,415, "Amsterdam");
-        Cruce c16 = new Cruce (500,520, "Viena");
-        Cruce c17 = new Cruce (340,475, "Luxemburgo");
-        Cruce c18 = new Cruce (355,545, "Berna");
-        Cruce c19 = new Cruce (575,400, "Varsovia");
+        Cruce c1 = new Cruce (20,40, "C1");
+        Cruce c2 = new Cruce (60,40, "C2");
+        Cruce c3 = new Cruce (20,80, "C3");
+        Cruce c4 = new Cruce (60,80, "C4");
+        Cruce c5 = new Cruce (120,80, "C5");
+        Cruce c6 = new Cruce (250,140, "C6");
+        Cruce c7 = new Cruce (230,400, "C7");
+        Cruce c8 = new Cruce (420,90, "C8");
+        Cruce c9 = new Cruce (600,300, "C9");
+        Cruce c10 = new Cruce (500,450, "C10");
+        Cruce c11 = new Cruce (700,400, "C11");
+        Cruce c12 = new Cruce (240,500, "C12");
+        Cruce c13 = new Cruce (625,45, "C13");
+        Cruce c14 = new Cruce (520,80, "C14");
+        Cruce c15 = new Cruce (150,470, "C15");
+        Cruce c16 = new Cruce (360,415, "C16");
+        Cruce c17 = new Cruce (732,470, "C17");
+        Cruce c18 = new Cruce (612,80, "C18");
 
         this.mapa.addCruce(c1);
         this.mapa.addCruce(c2);
@@ -63,58 +62,54 @@ public class Principal {
         this.mapa.addCruce(c16);
         this.mapa.addCruce(c17);
         this.mapa.addCruce(c18);
-        this.mapa.addCruce(c19);
-        
-        
+
         c1.addNeighbor(c2);
         c2.addNeighbor(c1);
-        c2.addNeighbor(c3);
-        c3.addNeighbor(c2);
+        c1.addNeighbor(c3);
+        c3.addNeighbor(c1);
         c2.addNeighbor(c4);
         c4.addNeighbor(c2);
         c3.addNeighbor(c4);
         c4.addNeighbor(c3);
+        c5.addNeighbor(c6);
+        c6.addNeighbor(c5);
         c4.addNeighbor(c5);
         c5.addNeighbor(c4);
-        c5.addNeighbor(c9);
-        c9.addNeighbor(c5);
+        c6.addNeighbor(c7);
+        c7.addNeighbor(c6);
         c6.addNeighbor(c8);
         c8.addNeighbor(c6);
-        c3.addNeighbor(c12);
-        c12.addNeighbor(c3);
-        c4.addNeighbor(c12);
-        c12.addNeighbor(c4);
-        c12.addNeighbor(c13);
-        c13.addNeighbor(c12);
-        c13.addNeighbor(c10);
-        c10.addNeighbor(c13);
-        c4.addNeighbor(c14);
-        c14.addNeighbor(c4);
-        c14.addNeighbor(c15);
-        c15.addNeighbor(c14);
-        c13.addNeighbor(c16);
-        c16.addNeighbor(c13);
-        c14.addNeighbor(c17);
-        c17.addNeighbor(c14);
-        c17.addNeighbor(c18);
-        c18.addNeighbor(c17);
+        c5.addNeighbor(c8);
+        c8.addNeighbor(c5);
+        c2.addNeighbor(c13);
+        c13.addNeighbor(c2);
+        c8.addNeighbor(c14);
+        c14.addNeighbor(c8);
+        c14.addNeighbor(c18);
+        c18.addNeighbor(c14);
         c13.addNeighbor(c18);
         c18.addNeighbor(c13);
-        c11.addNeighbor(c16);
-        c16.addNeighbor(c11);
-        c11.addNeighbor(c19);
-        c19.addNeighbor(c11);
-        c15.addNeighbor(c19);
-        c19.addNeighbor(c15);
-        
+        c18.addNeighbor(c11);
+        c11.addNeighbor(c18);
+        c7.addNeighbor(c16);
+        c16.addNeighbor(c7);
+        c10.addNeighbor(c11);
+        c11.addNeighbor(c10);
+        c10.addNeighbor(c16);
+        c16.addNeighbor(c10);
+        c16.addNeighbor(c12);
+        c12.addNeighbor(c16);
+        c12.addNeighbor(c15);
+        c15.addNeighbor(c12);
+        c10.addNeighbor(c17);
+        c17.addNeighbor(c10);
+              
         ArrayList<Cruce> prueba = new ArrayList<Cruce>();
         
         prueba.add(c1);
-        prueba.add(c3);
+        prueba.add(c16);
+        prueba.add(c12);
         prueba.add(c15);
-        prueba.add(c4);
-        prueba.add(c11);
-        prueba.add(c10);
         
         mapa.ruta = new Ruta(prueba);
         
